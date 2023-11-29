daf5e77667fc2205a91e9da291ed9829cdcbe555
Bram Mittendorff <bram@pescheck.nl>
2023-11-29 18:39:51 +0100

    Added first version of python-gpt-go

--- README.md
+++ README.md
@@ -1,15 +1,67 @@
-# Python ChatGPT Po File Converter
+# Python GPT-4 PO File Translator
 
-This script uses the `polib` library for processing `.po` files, `argparse` for command line argument parsing, and `os` for file system operations. It scans the specified input folder and its subfolders for `.po` files, checks if they match the given language, and optionally removes fuzzy entries based on the `--fuzzy` flag.
+This script provides an efficient way to translate `.po` files using OpenAI's GPT-4 model. It supports both bulk and one-by-one translation modes, making it versatile for different sizes and types of `.po` files.
 
-To use this script:
+## Features
 
-1. Ensure you have Python installed on your system.
-2. Install `polib` via pip: `pip install polib`.
-3. Save the script into a file, e.g., `po_scanner.py`.
-4. Run the script from the command line, providing the necessary arguments. For example:
+- **Bulk Translation**: Translate multiple entries at once for efficiency.
+- **One-by-One Translation**: Translate each entry separately for greater control.
+- **Adjustable Batch Size**: Control the number of translations processed in each batch during bulk translation.
+- **Logging**: Detailed logging for tracking progress and debugging.
+- **Fuzzy Entry Handling**: Option to ignore 'fuzzy' entries in `.po` files.
+
+## Requirements
+
+- Python 3.x
+- `polib` library
+- `openai` Python package
+- An API key from OpenAI
+
+## Installation
+
+1. Clone the repository:
    ```
-   python po_scanner.py /path/to/your/folder en --fuzzy
+   git clone [repository URL]
+   ```
+2. Install the required Python packages:
+   ```
+   pip install polib openai python-dotenv
    ```
 
-This command would scan for `.po` files in `/path/to/your/folder` that are in English (`en`) and remove fuzzy entries if they exist.
+## Configuration
+
+1. Create a `.env` file in the root directory of the project.
+2. Add your OpenAI API key to the `.env` file:
+   ```
+   OPENAI_API_KEY='your_api_key_here'
+   ```
+
+## Usage
+
+Run the script from the command line, providing the necessary arguments:
+
+```
+python po_translator.py --folder [path_to_po_files] --lang [language_codes] [--fuzzy] [--bulk] [--bulksize [batch_size]]
+```
+
+- `--folder`: The path to the folder containing `.po` files.
+- `--lang`: Comma-separated language codes to filter `.po` files for translation.
+- `--fuzzy`: (Optional) Set this flag to ignore entries marked as 'fuzzy'.
+- `--bulk`: (Optional) Set this flag to use bulk translation mode.
+- `--bulksize`: (Optional) Specify the number of translations to process in each batch (default is 50).
+
+Example:
+
+```
+python po_translator.py --folder ./locales --lang de,fr --bulk --bulksize 100
+```
+
+This will translate all `.po` files in the `./locales` folder to German and French in bulk mode, processing 100 translations per batch.
+
+## Logging
+
+The script logs its progress and any errors encountered. Logs provide details about the files being processed, the number of translations, and the current batch in bulk mode.
+
+## License
+
+(MIT)[LICENSE]
--- main.py
+++ main.py
@@ -1,65 +1,110 @@
 import argparse
 import os
+import time
 import polib
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
+import logging
 
-def translate_text(text, target_language):
-    """
-    Translate the given text to the target language using OpenAI's gpt-4-1106-preview model.
+# Load environment variables from .env file
+load_dotenv()
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-    :param text: Text to translate.
-    :param target_language: Language code to translate the text into.
-    :return: Translated text.
-    """
-    completion = openai.ChatCompletion.create(
-        model="gpt-4-1106-preview",
-        messages=[
-            {
-                "role": "user",
-                "content": f"Translate this into {target_language}: {text}",
-            },
-        ],
-    )
-    return completion.choices[0].message.content.strip()
+# Setting up logging
+logging.basicConfig(level=logging.INFO)
 
-def scan_and_process_po_files(input_folder, languages, fuzzy):
-    """
-    Scans all .po files in the specified folder and subfolders.
-    Translates and removes 'fuzzy' entries if specified.
+BATCH_SIZE = 50
 
-    :param input_folder: The root folder to start the scan.
-    :param languages: List of language codes to filter the .po files.
-    :param fuzzy: Boolean indicating whether to remove fuzzy entries.
-    """
+def translate_text_bulk(texts, target_language, po_file_path, current_batch, total_batches, is_bulk):
+    translated_texts = []
+    
+    for i in range(0, len(texts), BATCH_SIZE):
+        batch_texts = texts[i:i + BATCH_SIZE]
+        batch_info = f"File: {po_file_path}, Batch {current_batch + i // BATCH_SIZE}/{total_batches} (texts {i + 1}-{min(i + BATCH_SIZE, len(texts))})"
+        translation_request = f"Translate the following texts into {target_language}:\n\n" + "\n\n".join(batch_texts)
+        retries = 3
+
+        while retries > 0:
+            try:
+                if is_bulk:  # Only log detailed batch info for bulk translations
+                    logging.info(f"Translating {batch_info}.")
+                    logging.debug(f"Text to translate {batch_texts}")
+                    logging.debug(f"Retries {retries}")
+                completion = client.chat.completions.create(
+                    model="gpt-4-1106-preview",
+                    messages=[{"role": "user", "content": translation_request}])
+                batch_translations = completion.choices[0].message.content.strip().split('\n\n')
+                translated_texts.extend(batch_translations)
+                break  # Break the loop if successful
+            except Exception as e:
+                retries -= 1
+                logging.error(f"Error in translating {batch_info}: {e}. Retrying... {retries} attempts left.")
+                if retries <= 0:
+                    logging.error(f"Maximum retries reached for {batch_info}. Skipping this batch.")
+                    translated_texts.extend(['Error in translation'] * len(batch_texts))
+                time.sleep(1)  # Briefly pause before retrying
+
+    return translated_texts
+
+def scan_and_process_po_files(input_folder, languages, fuzzy, bulk_mode):
     for root, dirs, files in os.walk(input_folder):
         for file in files:
             if file.endswith(".po"):
-                po_file_path = os.path.join(root, file)
-                po_file = polib.pofile(po_file_path)
-                file_lang = po_file.metadata.get('Language', '')
-                if file_lang in languages:
-                    for entry in po_file:
-                        if not entry.msgstr and entry.msgid:
-                            entry.msgstr = translate_text(entry.msgid, file_lang)
-                    if fuzzy:
-                        po_file.remove_fuzzy_entries()
-                    po_file.save(po_file_path)
-                    print(f"Processed {po_file_path}")
+                try:
+                    po_file_path = os.path.join(root, file)
+                    po_file = polib.pofile(po_file_path)
+                    file_lang = po_file.metadata.get('Language', '')
+                    if file_lang in languages:
+                        texts_to_translate = [entry.msgid for entry in po_file if not entry.msgstr and entry.msgid and not (fuzzy and 'fuzzy' in entry.flags)]
+                        total_items = len(texts_to_translate)
+                        logging.info(f"Found {total_items} items to translate.")
+
+                        if bulk_mode:
+                            translate_in_bulk(texts_to_translate, file_lang, po_file, po_file_path)
+                        else:
+                            translate_one_by_one(texts_to_translate, file_lang, po_file, po_file_path)
+
+                        po_file.save(po_file_path)
+                        logging.info(f"Finished processing .po file: {po_file_path}")
+                except Exception as e:
+                    logging.error(f"Error processing file {po_file_path}: {e}")
+
+def translate_in_bulk(texts, target_language, po_file, po_file_path):
+    total_batches = (len(texts) - 1) // BATCH_SIZE + 1
+    translated_texts = translate_text_bulk(texts, target_language, po_file_path, 0, total_batches, True)
+    apply_translations_to_po_file(translated_texts, texts, po_file)
+
+def translate_one_by_one(texts, target_language, po_file, po_file_path):
+    for index, text in enumerate(texts):
+        logging.info(f"Translating text {index + 1}/{len(texts)} in file {po_file_path}")
+        translated_text = translate_text_bulk([text], target_language, po_file_path, index, len(texts), False)[0]
+        entry = po_file.find(text)  # Corrected this line
+        if entry:
+            entry.msgstr = translated_text
+
+def apply_translations_to_po_file(translated_texts, original_texts, po_file):
+    for original, translated in zip(original_texts, translated_texts):
+        if not translated.startswith("Error in translation"):
+            entry = po_file.find(original)  # Corrected this line
+            if entry:
+                entry.msgstr = translated
 
 def main():
-    load_dotenv()  # Load environment variables from .env file
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-
     parser = argparse.ArgumentParser(description="Scan and process .po files")
     parser.add_argument("--folder", required=True, help="Input folder containing .po files")
     parser.add_argument("--lang", required=True, help="Comma-separated language codes to filter .po files")
     parser.add_argument("--fuzzy", action="store_true", help="Remove fuzzy entries")
-    
-    args = parser.parse_args()
+    parser.add_argument("--bulk", action="store_true", help="Use bulk translation mode")
+    parser.add_argument("--bulksize", type=int, default=50, help="Batch size for bulk translation")
 
+    args = parser.parse_args()
     languages = [lang.strip() for lang in args.lang.split(',')]
-    scan_and_process_po_files(args.folder, languages, args.fuzzy)
+
+    # Set the global BATCH_SIZE based on the user input
+    global BATCH_SIZE
+    BATCH_SIZE = args.bulksize
+
+    scan_and_process_po_files(args.folder, languages, args.fuzzy, args.bulk)
 
 if __name__ == "__main__":
     main()
