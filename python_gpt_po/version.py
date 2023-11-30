6153ddc07b801830741010be19c4eaef6f558779
Bram Mittendorff <bram@pescheck.nl>
2023-11-30 15:20:02 +0100

    Added docstring version.py

--- python_gpt_po/version.py
+++ python_gpt_po/version.py
@@ -1,1 +1,22 @@
+"""
+version.py
+
+This module defines the version of the gpt-po-translator package.
+
+Attributes:
+    __version__ (str): A string representing the version number of the gpt-po-translator package.
+                      The versioning follows the Semantic Versioning scheme.
+
+Example:
+    A version string looks like '0.1.3', where:
+    - The first digit (0) represents the major version.
+    - The second digit (1) represents the minor version.
+    - The third digit (3) represents the patch number.
+
+Notes:
+    The version information is used in setup.py for package distribution and can also be used
+    within the application to display the current version to the user, for example, in a CLI
+    using the '--version' argument.
+"""
+
 __version__ = "0.1.3"
