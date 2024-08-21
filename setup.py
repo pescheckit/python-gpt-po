e98d4a9905414daf3bf9b70d1bf26fc83512d73e
Bram Mittendorff <bram@pescheck.nl>
2024-08-21 15:30:08 +0200

    Added automaticly tagging

--- setup.py
+++ setup.py
@@ -5,15 +5,14 @@ This script is used to install the package and its dependencies.
 
 from setuptools import find_packages, setup
 
-from python_gpt_po.version import __version__
-
 # Read the contents of README file
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='gpt-po-translator',
-    version=__version__,
+    use_scm_version=True,  # Automatically fetch version from git tags
+    setup_requires=['setuptools-scm'],  # Ensure setuptools-scm is used during setup
     author='Bram Mittendorff',
     author_email='bram@pescheck.io',
     description='A CLI tool for translating .po files using GPT models.',
@@ -35,7 +34,6 @@ setup(
         ],
     },
     classifiers=[
-        # Choose your license as you wish
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
