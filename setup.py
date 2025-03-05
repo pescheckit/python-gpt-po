642f9eb5b618dc7892c32384d76b694b097975c1
bram <bram@pescheck.io>
2025-03-05 11:28:11 +0100

    Added encoding

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import find_packages, setup
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-with open('requirements.txt') as f:
+with open('requirements.txt', encoding='utf-8') as f:
     install_requires = [line.strip() for line in f if line.strip() and not line.startswith('#')]
 
 setup(
