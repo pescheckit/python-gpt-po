48940b7c9d43c49e1220f28dccebdc9799cac001
bram <bram@pescheck.io>
2025-03-05 10:53:33 +0100

    Added also better setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,20 @@ This script is used to install the package and its dependencies.
 
 from setuptools import find_packages, setup
 
-# Read the contents of README file
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='gpt-po-translator',
-    use_scm_version=True,  # Automatically fetch version from git tags
-    setup_requires=['setuptools-scm==8.1.0'],  # Ensure setuptools-scm is used during setup
+    use_scm_version=True,
+    setup_requires=['setuptools-scm==8.1.0'],
     author='Bram Mittendorff',
     author_email='bram@pescheck.io',
     description='A CLI tool for translating .po files using GPT models.',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/pescheckit/python-gpt-po',
-    license='LICENSE',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -35,9 +34,25 @@ setup(
         ],
     },
     classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Localization',
+        'Topic :: Text Processing :: Linguistic',
+        'Operating System :: OS Independent',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
-        # Add additional classifiers as needed
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3 :: Only',
+        'Natural Language :: English',
+        'Natural Language :: Dutch',
+        'Environment :: Console',
+        'Typing :: Typed'
     ],
+    python_requires='>=3.8',
 )
